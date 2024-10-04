c591e6c382cdfbe0ac5ec7da274ab89e946de421
ujjwal <ujjwalbaranwal221715@gmail.com>
2024-10-05 01:03:31 +0530

    add the server and ghe model

---
+++ Backend/app.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const morgan = require("morgan");
+const express = require("express");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const app = express();
+
+module.exports = app;
